efecc0deda7229881de124fcd9413a27bc1dc892
Pavel Solomatin <crave_ozer_man@mail.ru>
2022-09-15 13:14:40 +0300

    Задание 2: первые два пункта

---
+++ task_2/apache/drawer.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Drawer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<h1>Рисовашки</h1>
+<p>Задайте число от 0 до 127 в параметре, чтобы нарисовать фигуру</p>
+<pre>
+    http:localhost:8080/drawer.php?num=...
+</pre>
+<?php
+if (isset($_GET['num'])) {
+    /*
+    num = b'sscccaa'
+    s - shape
+    c - color
+    a - size
+    */
+    $num = $_GET['num'];
+    $size = ($num & 3) + 1;
+    $red = ($num >> 4) & 1;
+    $green = ($num >> 3) & 1;
+    $blue = ($num >> 2) & 1;
+    $shape = ($num >> 5) & 3;
+    $color = "\"#".($red == 1 ? "ff" : "00").($green == 1 ? "ff" : "00").($blue == 1 ? "ff" : "00")."\"";
+    $shape_tag = "";
+    $scale = 50;
+    switch ($shape) {
+        case 0:
+            $r = ($size * $scale / 2);
+            $shape_tag = "circle cx=".($r + 10)." cy=".($r + 10)." r=".$r." ";
+            break;
+        case 1:
+            $shape_tag = "rect x=10 y=10 width=".($size * $scale * 2)." height=".($size * $scale)." ";
+            break;
+        case 2:
+            $shape_tag = "rect x=10 y=10 width=".($size * $scale)." height=".($size * $scale)." ";
+            break;
+        case 3:
+            $side = $size * $scale;
+            $shape_tag = "polygon points='".($side / 2 + 5).",10"." 10,".($side)." ".($side).",".($side)."' ";
+    }
+    echo "<svg width='1000' height='1000'>";
+    echo "  <".$shape_tag."fill=".$color." style='stroke:black'"."/>";
+    echo "</svg>";
+} else {
+    echo "<p>Задайте число от 0 до 127 в параметре, чтобы нарисовать фигуру</p>
+<pre>
+    http:localhost:8080/drawer.php?num=...
+</pre>";
+}
+
+?>
+</body>
+</html>
+
+
---
+++ task_2/apache/sort.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quicksort</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<h1>Сортировка массива</h1>
+<?php
+
+function quick_sort($array) {
+    if (count($array) < 2) {
+        return $array;
+    }
+    $target = $array[0];
+    $less = array(); $equal = array($target); $greater = array();
+    for ($i = 1; $i < count($array); $i++) {
+        $elem = $array[$i];
+        if ($elem > $target) {
+            $greater[] = $elem;
+        } elseif ($elem < $target) {
+            $less[] = $elem;
+        } else {
+            $equal[] = $elem;
+        }
+    }
+    $less = quick_sort($less);
+    $greater = quick_sort($greater);
+    return array_merge($less, $equal, $greater);
+}
+
+function print_array($array): void {
+    echo "<pre>[";
+    echo implode(", ", $array);
+    echo "]</pre>";
+}
+
+if (isset($_GET['array'])) {
+    $array = explode(",", $_GET["array"]);
+    echo "<p>Считанный массив</p>";
+    print_array($array);
+    $array = quick_sort($array);
+    echo "<p>Отсортированный массив</p>";
+    print_array($array);
+} else {
+    echo "<p>Задайте числа, разделённые запятыми в параметре array, чтобы отсортировать их</p>
+<pre>
+    http:localhost:8080/drawer.php?array=4,2,5, ...
+</pre>";
+}
+?>
+</body>
+</html>
+
