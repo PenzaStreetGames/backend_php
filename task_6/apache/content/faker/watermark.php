5d190f5ee18fabfb0aa5a2a307868097e2ba005f
Pavel Solomatin <crave_ozer_man@mail.ru>
2022-11-14 02:07:25 +0300

    Задание 6: Заплатка

--- task_6/apache/content/faker/watermark.php
+++ task_6/apache/content/faker/watermark.php
@@ -8,12 +8,34 @@ function add_watermark($image)
     $image1 = imagecreatefromstring(file_get_contents($image1));
     $image2 = imagecreatefromstring(file_get_contents($image2));
 
-    # Не работает полупрозрачность
-    imagecolorallocate($image1, 255, 255, 255);
-    imagesavealpha($image1, true);
-    imagecolorallocatealpha($image2, 255, 255, 255, 127);
+    imagealphablending($image2, false);
     imagesavealpha($image2, true);
+    $alpha = round(127/255*127); // convert to [0-127]
 
-    imagecopymerge($image1, $image2, 50, 50, 0, 0, $width, $height, 100);
+    for ($x = 0; $x < $width; $x++) {
+        for ($y = 0; $y < $height; $y++) {
+
+            // get current color (R, G, B)
+            $rgb = imagecolorat($image2, $x, $y);
+            $r = ($rgb >> 16) & 0xff;
+            $g = ($rgb >> 8) & 0xff;
+            $b = $rgb & 0xf;
+
+            // create new color
+            $col = imagecolorallocatealpha($image2, $r, $g, $b, $alpha);
+
+            // set pixel with new color
+            imagesetpixel($image2, $x, $y, $col);
+        }
+    }
+
+    # Не работает полупрозрачность
+//    imagecolorallocate($image1, 255, 255, 255);
+//    imagesavealpha($image1, true);
+//    $color = imagecolorallocatealpha($image2, 255, 255, 255, 127);
+//    imagecolorset($image2, )
+//    imagesavealpha($image2, true);
+
+    imagecopy($image1, $image2, 50, 50, 0, 0, $width, $height);
     imagepng($image1, $image);
 }
